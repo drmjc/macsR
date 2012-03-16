ab9e65e5e25ef6d85021f3260da7712a16984cd9
Mark Cowley <m.cowley0@gmail.com>
2012-03-16 02:48:16 +0000

    v1

---
+++ inst/bin/fix.macs.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+# Fix a MACS output file.
+# 
+# usage:
+#
+# Mark Cowley, 2012-03-13
+
+usage()
+{
+cat << EOF
+usage: $0 options
+
+This script takes a MACS result directory & annotates the peaks with nearest TSS and CpG islands.
+
+OPTIONS:
+     -h, -?: help
+EOF
+}
+
+if [ $# -eq 0 ]; then 
+  usage
+  exit 1
+fi
+
+while getopts "h" option
+do
+     case "${option}" in
+          h) usage
+             exit 1;;
+          \?) usage
+             exit 1;;
+     esac
+done
+# update $@ to not include any of these options.
+shift $(($OPTIND - 1)) 
+
+dir=${1-"."}
+if [ ! -d "$dir" ]; then 
+	echo "ERROR: input should be a directory: $dir"
+	usage
+	exit 2
+fi
+
+cd "$dir"
+echo "Annotating MACS results in `pwd`"
+Rscript --vanilla -e 'suppressPackageStartupMessages(library(macsR)); fix.macs.output(getwd(), fdr.thresh=c(5,10,15,25))' || { echo >&2 "fix.macs.output failed. Aborting"; exit 2; }
+cd -
