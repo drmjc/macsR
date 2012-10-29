845ad97030d47087d1dcd5139ffc531241a6b77a
Mark Cowley <m.cowley0@gmail.com>
2012-10-29 00:12:15 +0000

    collapsed CpG data annotation RD's into 1 file

--- R/CpG-data.R
+++ R/CpG-data.R
@@ -1,71 +1,29 @@
-#' @name CpG.human.GRCh37
-#' @title CpG islands - human - genome build: GRCh37/hg19
-#' @description This data set list the genomic locations of human CpG islands,
-#' with coordinates based on the GRCh37 / hg19 genome build.
+#' @name CpG.island.data
+#' @title CpG islands
+#' @rdname CpG.data
+#' @description These datasets list the genomic locations of CpG islands from the
+#' named species, with coordinates based on the genome build. They are named: \code{Cpg.org.build},
+#' eg \code{CpG.human.GRCh37}, or \code{CpG.mouse.NCBI37}
+#' 
 #' @docType data
+#' @keywords datasets
+#' 
 #' @usage CpG.human.GRCh37
-#' @format a \code{RangedData} instance, 1 row per CpG island.
-#' @source UCSC Table Browser
-#' @author Mark Cowley, 2012-03-05
-#' @aliases CpG.human.hg19
-NULL
-
-#' @name CpG.human.NCBI36
-#' @title CpG islands - human - genome build: NCBI36/hg18
-#' @description This data set list the genomic locations of human CpG islands,
-#' with coordinates based on the NCBI36 / hg18 genome build.
-#' @docType data
 #' @usage CpG.human.NCBI36
-#' @format a \code{RangedData} instance, 1 row per CpG island.
-#' @source UCSC Table Browser
-#' @author Mark Cowley, 2012-03-05
-#' @aliases CpG.human.hg18
-NULL
-
-#' @name CpG.human.NCBI35
-#' @title CpG islands - human - genome build: NCBI35/hg17
-#' @description This data set list the genomic locations of human CpG islands,
-#' with coordinates based on the NCBI35 / hg17 genome build.
-#' @docType data
 #' @usage CpG.human.NCBI35
-#' @format a \code{RangedData} instance, 1 row per CpG island.
-#' @source UCSC Table Browser
-#' @author Mark Cowley, 2012-03-05
-#' @aliases CpG.human.hg17
-NULL
-
-#' @name CpG.mouse.NCBI37
-#' @title CpG islands - mouse - genome build: NCBI37/mm9
-#' @description This data set list the genomic locations of mouse CpG islands,
-#' with coordinates based on the NCBI37 / mm9 genome build.
-#' @docType data
+#' @usage CpG.human.hg17
+#' @usage CpG.human.hg18
+#' @usage CpG.human.hg19
 #' @usage CpG.mouse.NCBI37
-#' @format a \code{RangedData} instance, 1 row per CpG island.
-#' @source UCSC Table Browser
-#' @author Mark Cowley, 2012-03-05
-#' @aliases CpG.mouse.mm9
-NULL
-
-#' @name CpG.mouse.NCBI36
-#' @title CpG islands - mouse - genome build: NCBI36/mm8
-#' @description This data set list the genomic locations of mouse CpG islands,
-#' with coordinates based on the NCBI36 / mm8 genome build.
-#' @docType data
 #' @usage CpG.mouse.NCBI36
-#' @format a \code{RangedData} instance, 1 row per CpG island.
-#' @source UCSC Table Browser
-#' @author Mark Cowley, 2012-03-05
-#' @aliases CpG.mouse.mm8
-NULL
-
-#' @name CpG.rat.RGSC3.4
-#' @title CpG islands - rat - genome build: RGSC3.4/rn4
-#' @description This data set list the genomic locations of rat CpG islands,
-#' with coordinates based on the RGSC3.4 / rn4 genome build.
-#' @docType data
+#' @usage CpG.mouse.mm8
+#' @usage CpG.mouse.mm9
 #' @usage CpG.rat.RGSC3.4
+#' @usage CpG.rat.rn4
+#' 
+#' @aliases CpG pG.human.hg19 CpG.human.hg18 CpG.human.hg17 CpG.mouse.mm9 CpG.mouse.mm8 CpG.rat.rn4 CpG.human.GRCh37 CpG.human.NCBI36 CpG.human.NCBI35 CpG.mouse.NCBI37 CpG.mouse.NCBI36 CpG.rat.RGSC3.4
+#' 
 #' @format a \code{RangedData} instance, 1 row per CpG island.
 #' @source UCSC Table Browser
 #' @author Mark Cowley, 2012-03-05
-#' @aliases CpG.rat.rn4
 NULL
