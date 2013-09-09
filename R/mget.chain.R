4f6747bbdf1ce87b204d84df37fc8a340e5f4009
Mark Cowley <m.cowley@garvan.org.au>
2013-09-09 22:35:28 +1000

    Replaced symlinks with hardlinks
    Github does not follow symlinks

--- R/mget.chain.R
+++ R/mget.chain.R
@@ -1,1 +1,53 @@
-/Users/marcow/src/R/microarrays/R/mget.chain.R
+#' Lookup keys in a chain of 2 environment or AnnDbBimap objects
+#' 
+#' Sometimes you need to chain (or JOIN) a few mapping tables together
+#' in order to get the end result.
+#' For example, Using the annotation.db packages, if you need to go from
+#' GENESYMBOL to GENENAME, you have to go via the ENTREZGENE ID.
+#' You would lookup the entrez gene ID for
+#' each SYMBOL, using the org.Hs.egSYMBOL2EG AnnDbBimap, 
+#' which can sometimes return \code{NA}'s. Then take
+#' those Entrez Gene ID's and look them up in the org.Hs.egGENENAME
+#' table. This function does the hard work for you.
+#' @note this returns only the first match for each key, along each of 
+#' the mapping steps, where \dQuote{first} is defined in \code{\link{mget2}}.
+#' 
+#' @param keys a vector of search keys which must be found in a
+#' @param a an environment, or AnnDbBimap object
+#' @param b an environment, or AnnDbBimap object
+#' @param debug logical: if \code{TRUE}, then a 3 column \code{data.frame} is
+#' returned, containing the keys, intermediate result, then final value. If 
+#' \code{FALSE}, the default, return a named vector of values
+#' @return For the N search keys, either return a named vector, of length N of mapped 
+#' values, or a 3 column, N row \code{data.frame}, if
+#' \code{debug=TRUE}. if there is no mapped value for a key, \code{NA} is 
+#' returned.
+#' @author Mark Cowley, 2011-09-21
+#' @export
+#' @examples
+#' if( require(org.Hs.eg.db) ) {
+#'   geneids <- c("TP53", "INS", "KRAS")
+#'   mget.chain(geneids, org.Hs.egSYMBOL2EG, org.Hs.egGENENAME, FALSE)
+#'   mget.chain(geneids, org.Hs.egSYMBOL2EG, org.Hs.egGENENAME, TRUE)
+#' }
+#' 
+mget.chain <- function(keys, a, b, debug=FALSE) {
+	length(intersect(keys, Rkeys(a))) > 0 || stop("none of your keys found in first env/map")
+	length(intersect(Lkeys(a), Lkeys(b))) > 0 || stop("a and b are incompatible - no overlaps found")
+	map <- data.frame(SearchKeys=keys, intermediate=NA, value=NA)
+	map$intermediate <- mget2(keys, a, first=TRUE, na.rm=FALSE)
+	
+	idx <- !is.na(map$intermediate)
+	a2b <- mget2(map$intermediate[idx], b, first=TRUE, na.rm=FALSE)
+	map$value[idx] <- a2b
+	
+	if( debug ) {
+		res <- map
+	}
+	else {
+		res <- map[,3]
+		names(res) <- map[,1]
+	}
+	
+	res
+}
--- R/mget2.R
+++ R/mget2.R
@@ -1,1 +1,60 @@
-/Users/marcow/src/R/microarrays/R/mget2.R
+#' enhanced mget
+#' 
+#' Enchanced mget to look up x in y. Changes in addition to \code{\link{mget}}
+#' include:\cr
+#' * Values can be sorted alphanumerically\cr
+#' * Since some keys have multiple values, this function returns all, just
+#' the first (hint; use \code{first=TRUE, sort=TRUE}), assuming that the first
+#' is the oldest, or all values collapsed into a single string. \cr
+#' * keys in \code{x} are allowed to be \code{NA} (hint: \code{na.rm=FALSE})
+#' 
+#' @section TODO:
+#' provide more sorting options to choose the newest.
+#' 
+#' @param x a vector of keys, \code{NA}'s allowed.
+#' @param y an env, or AnnDbBimap, or ProbeAnnDbBimap
+#' @param sort logical: if \code{TRUE}, then sort the values for each key
+#' @param first logical: if \code{TRUE}, then choose the first value for 
+#'  each key, after the optional sort
+#' @param na.rm logical: remove those keys that had no matches? if \code{TRUE}, 
+#'   a vector with no \code{NA}'s will be returned. if \code{FALSE}, then return
+#'   a list of key to value maps, possibly with \code{NA}'s.
+#' @param collapse specify a character(1), eg \dQuote{", "} to collapse multiple values\cr
+#'  into a single \code{character}. Useful if you want to report all multi-mapping values.
+#'  Note that this is ignored if first=TRUE, and that this will always return a character
+#' 
+#' @return either a vector or list of values, depending on the value of 
+#'  \code{na.rm}, and collapse
+#' 
+#' @author Mark Cowley, 2011-08-25
+#' 
+#' @export
+#' @importFrom AnnotationDbi mget
+#' 
+#' @examples
+#' if( require(org.Hs.eg.db) ) {
+#'   sym <- c("TP53", "INS", "EGFR", "MET", NA)
+#'   mget2(sym, org.Hs.egSYMBOL2EG)
+#'   mget2(sym, org.Hs.egSYMBOL2EG, na.rm=FALSE)
+#' }
+#' 
+mget2 <- function(x, y, sort=TRUE, first=TRUE, na.rm=TRUE, collapse=NULL) {
+	idx <- !is.na(x)
+	length(idx)>0 || return(NA)
+	
+	res <- rep(NA, length(x))
+	res[idx] <- mget(x[idx], y, ifnotfound=NA)
+	if( sort )  res <- lapply(res, sort)
+	if( first ) res <- lapply(res, "[", 1)
+	else if ( !is.null(collapse) && is.character(collapse) && length(collapse) == 1 ) res <- sapply(res, paste, collapse=collapse) 
+	if( na.rm ) res <- res[!is.na(res)]
+	
+	res <- unlist(res)
+	res	
+}
+# 2011-12-30
+# - allow NA's in x
+# 2012-03-06
+# - removed dependence on pwbc::na.rm, for improved portability
+# 2013-07-23
+# - added collapse parameter.
